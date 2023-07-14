89f7b47d780a8216d8fde186b7cfb22645d10938
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-07-13 20:34:45 -0500

    Fixing Folder/node modules

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Navbar from './components/navbar';
+import HomePage from './components/homepage';
+import Adopt from './components/adopt';
+import Food from './components/food';
+import Locations from './components/locations';
+import ExoticPets from './components/exotic-pets';
 
 function App() {
+  const [page, setPage] = useState('Homepage');
+
+  const renderPage = () => {
+    if (page === 'Adopt') {
+      return <Adopt />;
+    } else if (page === 'Navbar') {
+      return <Navbar />;
+    } else if (page === 'Exotic-pets') {
+      return <ExoticPets />;
+    } else if (page === 'Locations') {
+      return <Locations />;
+    } else if (page === 'Food') {
+      return <Food />;
+    } else {
+      // Default page or handle other cases
+      return <HomePage />;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar setPage={setPage} />
+      {renderPage()}
     </div>
   );
 }
